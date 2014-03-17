ebac8277ff37e410f166bbb454bca4f990219ca3
adrien-louis-r <adrien.louis.r@gmail.com>
2014-03-17 10:07:22 +0100

    Export route and database config from server.js

--- app/routes.js
+++ app/routes.js
@@ -1,5 +1,32 @@
-module.exports = function(app) {
+module.exports = function(app, passport) {
 	app.get('/', function(req, res) {
 		res.render('../app/views/index.ejs')
 	});
+
+	app.get('/login', function(req, res) {
+		res.render('../app/views/login.ejs');
+	});
+
+	app.get('/signup', function(req, res) {
+		res.render('../app/views/signup.ejs');
+	});
+
+	app.get('/profile', isLoggedIn, function(req, res) {
+		res.render('../app/views/profile.ejs', {
+			user : req.user
+		});
+	});
+
+	app.get('logout', function(req, res) {
+		req.logout();
+		res.redirect('/');
+	});
+
+	function isLoggedIn(req, res, next) {
+		if (req.isAuthenticated()) {
+			return next();
+		} else {
+			res.redirect('/');
+		}
+	}
 }
--- server.js
+++ server.js
@@ -1,18 +1,34 @@
 // set up
-var express  = require('express');
-var app      = express();
-var mongoose = require('mongoose');
-var port = Number(process.env.PORT || 3000)
-require('./app/routes')(app);
+var express  = require('express'),
+	app = express(),
+	mongoose = require('mongoose'),
+	port = Number(process.env.PORT || 3000),
+	configDB = require('./config/database.js');
 
-// configuration
-//mongoose.connect('mongodb://localhost/test');
-//
-app.configure(function() {
-	app.use(express.static(__dirname + '/public'));
+mongoose.connect(configDB.url);
+
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function callback () {
+	console.log('Connected to db on mongodb://localhost/stonejs');
 });
 
-// listen (start app with node server.js)
-var server = app.listen(port, function() {
-    console.log('Listening on port %d', server.address().port);
+app.configure(function() {
+	app.use(express.logger('dev')); // log every request to the console
+	app.use(express.cookieParser()); // read cookies (needed for auth)
+	app.use(express.bodyParser()); // get information from html forms
+
+	app.set('view engine', 'ejs'); // set up ejs for templating
+
+	app.use(express.session({ secret: 'thisisaverylongandcomplicatedpassphrase!' })); // session secret
+	app.use(passport.initialize());
+	app.use(passport.session()); // persistent login sessions
+	// app.use(flash()); // use connect-flash for flash messages stored in session
+
+});
+
+require('./app/routes.js')(app, passport); // load our routes and pass in our app and fully configured passport
+
+app.listen(port, function() {
+	console.log('Listening on http://127.0.0.1:%d', server.address().port);
 });
